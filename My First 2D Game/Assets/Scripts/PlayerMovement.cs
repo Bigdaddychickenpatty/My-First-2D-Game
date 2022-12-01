eaeb9212bb2127d6eeab15910ea4e1a5392728e8
Bigdaddychickenpatty <111007609+Bigdaddychickenpatty@users.noreply.github.com>
2022-12-01 11:05:17 -0800

    2nd tilemap

--- My First 2D Game/Assets/Scripts/PlayerMovement.cs
+++ My First 2D Game/Assets/Scripts/PlayerMovement.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 10;
-    public float jumpForce = 5;
+    public float speed = 5;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +15,11 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+
+        transform.Translate(Vector3.up * verticalInput * speed * Time.deltaTime);
     }
 }
