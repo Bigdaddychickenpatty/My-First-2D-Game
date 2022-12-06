2eff1977fa285bcd362488cb679556b99318f7af
Bigdaddychickenpatty <111007609+Bigdaddychickenpatty@users.noreply.github.com>
2022-12-06 11:05:28 -0800

    Player flip

--- My First 2D Game/Assets/Scripts/PlayerMovement.cs
+++ My First 2D Game/Assets/Scripts/PlayerMovement.cs
@@ -5,21 +5,28 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5;
+    private Rigidbody2D _myrigidbody;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _myrigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
         transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
 
         transform.Translate(Vector3.up * verticalInput * speed * Time.deltaTime);
+    }
+
+    void FlipSprite()
+    {
+        
     }
 }
