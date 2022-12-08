64f8e5b0797e8de52744937a97ae83fbab987fe5
Bigdaddychickenpatty <111007609+Bigdaddychickenpatty@users.noreply.github.com>
2022-12-08 11:05:15 -0800

    player turning

--- My First 2D Game/Assets/Scripts/PlayerMovement.cs
+++ My First 2D Game/Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 5;
+    public float Speed = 5;
     private Rigidbody2D _myrigidbody;
 
     // Start is called before the first frame update
@@ -16,17 +16,26 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-
-        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
-
-        transform.Translate(Vector3.up * verticalInput * speed * Time.deltaTime);
+       Playermovement();
+       FlipSprite(); 
     }
 
     void FlipSprite()
     {
+        bool ploayerHasHorizontalSpeed = Mathf.Abs(_myrigidbody.velocity.x) > Mathf.Epsilon;
         
+        if (ploayerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(_myrigidbody.velocity.x), 1f);
+        }
+    }
+
+    void Playermovement()
+    {
+         float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+         FlipSprite();
+        _myrigidbody.velocity = new Vector2(horizontalInput * Speed, verticalInput * Speed);
     }
 }
